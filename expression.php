f5bbdc45629cd74289e6815e073830ada831a933
ALichrieff <alichrief12gmail.com>
2022-09-13 09:20:43 +0300

    perfrom arithmetic expression using _POST method

---
+++ expression.php
@@ -0,0 +1,13 @@
+<?php
+$results=0;
+if (isset($_POST["a"]) && isset($_POST["b"]) && isset($_POST["c"])) {
+    $a=$_POST["a"];
+    $b=$_POST["b"];
+    $c=$_POST["c"];
+    $results=pow($a,3) + ($b*3)-($a/$b);
+}
+else{
+     echo "not enough variables"
+}
+
+?>
